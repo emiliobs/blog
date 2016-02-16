2befc9dcf7944b50c51abc0ecab347127d6f0363
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2016-02-16 14:33:19 +0100

    AppWebPHP - Blog
    Aplicación Web  - Blog - PHP

---
+++ app/UsuarioInc.php
@@ -0,0 +1,87 @@
+<?php   
+    
+class  Usuario 
+{
+    //Atributos:
+    private  $Id;
+    private  $Nombre;
+    private  $Email;
+    private  $password;
+    private  $FechaRegistro;
+    private  $Activo;
+            
+    //Constructor:
+    public function __construct($id,$nombre,$email,$password,$fechaRegistro,$activos) 
+    {
+        $this->Id            = $id;
+        $this->Nombre        = $nombre;
+        $this->Email         = $email;
+        $this->password      = $password;
+        $this->FechaRegistro = $fechaRegistro;
+        $this->Activo        = $activos;
+    }
+    
+    //Propiedades:
+    
+    //Getter
+    public function obtenerId()
+    {
+        return $this->Id;
+    }
+    
+    public function obtenerNombre()
+    {
+        return $this->Nombre;
+    }
+    
+    public function obtenerEmail()
+    {
+        return $this->Email;
+    }
+    
+    public function obtenerPassword()
+    {
+        return $this->Password;
+    }
+    
+    public function obtenerFechaRegistro()
+    {
+        return $this->FechaRegistro;
+    }
+    
+    public function obtenerActivo()
+    {
+        return $this->Activo;
+    }
+     
+    //Setter
+    public function cambiarNombre($nombre)
+    { 
+        $this->Nombre = $nombre;
+    }
+    
+    public function cambiarEmail($email)
+    { 
+        $this->Email = $email;
+    }
+    
+    public function cambiarPassword($password)
+    { 
+        $this->Password = $password;
+    }       
+    
+//    public function cambiarFechaRegistro($fechaRegistro)
+//    { 
+//        $this->FechaRegistro = $fechaRegistro;
+//    }       
+    
+    public function cambiarActivo($activo)
+    { 
+        $this->Activo = $activo;
+    }       
+
+}
+
+
+    
+?>
---
+++ app/config.inc.php
@@ -0,0 +1,6 @@
+<?php   
+    $config['nombreServidor'] = 'localhost';
+    $config['nombreUsuario'] = 'root';
+    $config['password'] = 'Emilio55';
+    $config['nombreBaseDatos'] = 'blog';
+?>
